2b3bfc9b3687add013a5b951f760cd5a3a546005
BourgondAries <macocio@gmail.com>
2015-10-18 15:49:51 +0200

    Make spaces into tabs

--- lengder.m
+++ lengder.m
@@ -5,9 +5,9 @@ svar=zeros(nelem,1);
 
 % Beregner elementlengder
 for i=1:nelem
-   dx = knutepunkt(element(i,1),1)-knutepunkt(element(i,2),1);
-   dy = knutepunkt(element(i,1),2)-knutepunkt(element(i,2),2);
-   svar(i) = sqrt(dx*dx + dy*dy);
+	dx = knutepunkt(element(i,1),1)-knutepunkt(element(i,2),1);
+	dy = knutepunkt(element(i,1),2)-knutepunkt(element(i,2),2);
+	svar(i) = sqrt(dx*dx + dy*dy);
 end
 
 
