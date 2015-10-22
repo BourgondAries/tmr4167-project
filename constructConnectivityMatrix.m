ba19091cd5037114159ef19744c2ec64a8074cb3
BourgondAries <macocio@gmail.com>
2015-10-22 14:09:17 +0200

    Add a connectivity matrix construction algorithm

---
+++ constructConnectivityMatrix.m
@@ -0,0 +1,10 @@
+function [matrix] = constructConnectivityMatrix(nodes, beams)
+	matrix = [];
+	for i=1:size(beams)
+		elem = beams{i, 1};
+		np1 = beams{i, 2};
+		np2 = beams{i, 3};
+		matrix = [matrix; elem np1 np2];
+	end
+	matrix = transpose(matrix);
+end
