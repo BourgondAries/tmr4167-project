884f650fe556c120c826cdfa9b88703bff28a110
BourgondAries <macocio@gmail.com>
2015-10-27 10:38:17 +0100

    Make the locals into a 3d array.

--- constructConnectivityMatrix.m
+++ constructConnectivityMatrix.m
@@ -1,9 +1,9 @@
 function [matrix] = constructConnectivityMatrix(beams)
 	matrix = [];
 	for i=1:size(beams)
-		elem = beams{i, 1};
-		np1 = beams{i, 2};
-		np2 = beams{i, 3};
+		elem = beams(i, 1);
+		np1 = beams(i, 2);
+		np2 = beams(i, 3);
 		matrix = [matrix; elem np1 np2];
 	end
 	matrix = transpose(matrix);
--- constructStiffnessMatrix.m
+++ constructStiffnessMatrix.m
@@ -1,11 +1,13 @@
 function [matrix] = constructStiffnessMatrix(connectivity, locals)
-	matrix = zeros(10, 10);
+	% Need to find the global matrix size by looking at the largest node.
+	largest = max(connectivity(1, :));
+	matrix = zeros(largest);
 	for i = 1:size(connectivity, 2)
 		start = connectivity(2, i);
 		stop = connectivity(3, i);
-		matrix(start, start) = matrix(start, start) + locals(1, 1);
-		matrix(start, stop) = matrix(start, stop) + locals(1, 2);
-		matrix(stop, start) = matrix(stop, start) + locals(2, 1);
-		matrix(stop, stop) = matrix(stop, stop) + locals(2, 2);
+		matrix(start, start) = matrix(start, start) + locals(1, 1, 1);
+		matrix(start, stop) = matrix(start, stop) + locals(1, 1, 2);
+		matrix(stop, start) = matrix(stop, start) + locals(1, 2, 1);
+		matrix(stop, stop) = matrix(stop, stop) + locals(1, 2, 2);
 	end
 end
