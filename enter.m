333fb35334473bc7c77421e085ecf1f56ebfe0bf
BourgondAries <macocio@gmail.com>
2015-10-27 11:06:20 +0100

    Fix the computation of the i values

--- enter.m
+++ enter.m
@@ -2,10 +2,12 @@ function [] = enter()
 	[nodes, beams, mats, pipes, boxes, qloads, ploads, incload] = lesinput();
 	constructConnectivityMatrix(beams);
 
-	createGeometries(pipes, boxes)
-
+	geoms = createGeometries(pipes, boxes);
 	beams = assignBeamLength(beams, nodes);
 	beams = assignBeamElasticity(beams, mats);
+	beams = assignBeamSecondMomentArea(beams, geoms)
+
+
 
 	%{
 	constructStiffnessMatrix(...
