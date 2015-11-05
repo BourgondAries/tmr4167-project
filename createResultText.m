be160e40eb89043424c1ed5494218d59a6b2a834
BourgondAries <macocio@gmail.com>
2015-11-05 12:31:25 +0100

    Introduce nice formatting.

--- createResultText.m
+++ createResultText.m
@@ -6,20 +6,20 @@ function text = createResultText(moments, shear, tension)
 	ten = 'Tension: ';
 	text = '';
 	for i = 1:size(moments, 2)
-		beamn = strcat(beam, num2str(i), '\n');
+		beamn = strcat('\n', beam, num2str(i), '\n');
 
-		node1 = strcat(node, '1\n');
+		node1 = strcat('----------\n', node, '1\n');
 		mom1 = strcat(mom, num2str(moments(1, i)), '\n');
 		shr1 = strcat(shr, num2str(shear(i, 1)), '\n');
 		ten1 = strcat(ten, num2str(tension(1, i)), '\n');
 
-		node2 = strcat(node, '2\n');
+		node2 = strcat('----------\n', node, '2\n');
 		mom2 = strcat(mom, num2str(moments(2, i)), '\n');
 		shr2 = strcat(shr, num2str(shear(i, 2)), '\n');
 		ten2 = strcat(ten, num2str(tension(2, i)), '\n');
 
 
-		node3 = strcat(node, 'middle/max\n');
+		node3 = strcat('----------\n', node, 'middle/max\n');
 		maxmoment = max(abs([moments(3, i) moments(4, i)]));
 		mom3 = strcat(mom, num2str(maxmoment), '\n');
 		maxtension = max(abs([tension(3, i) tension(4, i)]));
