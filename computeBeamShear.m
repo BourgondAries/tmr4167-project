bd48d51ab51cd6795477efbeaea7c1e6374c46bb
BourgondAries <macocio@gmail.com>
2015-11-06 01:31:09 +0100

    Fix the shear calculation.

--- computeBeamShear.m
+++ computeBeamShear.m
@@ -1,28 +1,28 @@
 
-% Beregner skjærkraft fra jevnt fordelte laster. 
+% Beregner skjærkraft fra jevnt fordelte laster.
 function shear = computeBeamShear(qloads, beamsize)
 	% Lager en tom matrise.
 	shear = zeros(beamsize, 2);
 	for i = 1:size(qloads)
-		
+
 		% Identifiserer hvilke elmement det virker jevnt forelte laster på.
 		beamid = qloads(i, 2);
 		length = qloads(i, 10);
-		
-		% Definerer størrelsen på laster, hentes fra matrisen qloads. 
+
+		% Definerer størrelsen på laster, hentes fra matrisen qloads.
 		qx = qloads(i, 3);
 		qy = qloads(i, 4);  % Always zero
 		assert(qy == 0);
 		qz = qloads(i, 5);
-		
-		% Definerer lastintensitet i ende1 og ende2 av bjelken. 
+
+		% Definerer lastintensitet i ende1 og ende2 av bjelken.
 		node1 = qloads(i, 6);
 		node2 = qloads(i, 7);
-		
+
 		dx = qloads(i, 8);
 		dz = qloads(i, 9);
 		% Bruker samme metode som tidligere, ser bort fra projeksjon og
-		% bruker kryssprodukt for å bestemme retning. 
+		% bruker kryssprodukt for å bestemme retning.
 		projection = [dx dz] * [qx; qz];
 		q = [qx qz] - projection * [dx dz];
 		neg = cross([q(1) 0 q(2)], [dx 0 dz]);
--- computePointShear.m
+++ computePointShear.m
@@ -1,6 +1,6 @@
-% Beregner skjærkraft som følge av punktlaster. 
+% Beregner skjærkraft som følge av punktlaster.
 function shear = computePointShear(ploads, beamsize)
-	
+
 	shear = zeros(beamsize, 2);
 	for i = 1:size(ploads)
 		beamid = ploads(i, 2);
@@ -9,23 +9,25 @@ function shear = computePointShear(ploads, beamsize)
 		px = ploads(i, 3);
 		py = ploads(i, 4);  % Always zero
 		assert(py == 0);
-		
+
 		%Definerer lasten i z-retning.
 		pz = ploads(i, 5);
 		node1 = ploads(i, 7);
 		node2 = ploads(i, 8);
-		
+
 		% Beregner lengde, neglisjerer projeksjon.
 		dx = ploads(i, 9);
 		dz = ploads(i, 10);
 		projection = [dx dz] * [px; pz];
 		p = [px pz] - projection * [dx dz];
-		
+
 		% Tar kryssproduktet for å finne retning.
 		neg = cross([p(1) 0 p(2)], [dx 0 dz]);
 		neg = neg(2);
-		
-		
-		shear(beamid, :) = [-neg/2];
+
+		a = ploads(i, 6);
+		L = ploads(i, 6);
+
+		shear(beamid, :) = [-neg/2*a*(L-a)];
 	end
 end
